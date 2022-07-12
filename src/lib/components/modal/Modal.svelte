b90bf203cb503ec3c9dcdedc22337d38aacce782
Simon Éveillé <eveillesimon@gmail.com>
2022-07-12 20:04:24 +0200

    Init modal component, edit on .gitignore

---
+++ src/lib/components/modal/Modal.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let id;
+    export let opened = true;
+
+    function close() {
+        opened = false;
+    }
+</script>
+
+<div id="{id}"
+     class="fixed z-50 left-0 top-0 w-screen h-screen overflow-auto bg-black/75 "
+     class:hidden={!opened}
+>
+    <div class="bg-white">
+        <button on:click={close}>Close Modal</button>
+    </div>
+</div>
---
+++ src/routes/components/modal.svelte
@@ -0,0 +1,6 @@
+<script>
+import Modal from "$lib/components/modal/Modal.svelte";
+</script>
+
+<h3>This is an example of a modal component</h3>
+<Modal/>
